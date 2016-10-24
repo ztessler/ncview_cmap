a6dedb79f9bd565e83045bc1b8a3eb3046e7b0e2
Zachary Tessler <ztessler@gmail.com>
2016-10-24 16:13:02 -0400

    option to add light and dark bands to the colormap

--- ncview_cmap.py
+++ ncview_cmap.py
@@ -1,14 +1,44 @@
 from __future__ import print_function
 import sys
+import argparse
+import colorsys
 import numpy as np
 import matplotlib.cm as cm
 
-if len(sys.argv) > 1:
-    cmap_name = sys.argv[1]
-else:
-    cmap_name = 'viridis'
+parser = argparse.ArgumentParser(description='Export matplotlib cmap in ncview format.')
+parser.add_argument('cmap', help='matplotlib colormap name')
+parser.add_argument('-b', '--bands', action='store_true', help='Add light and dark banding to cmap')
+args = parser.parse_args()
 
-cmap = getattr(cm, cmap_name)
+try:
+    cmap = getattr(cm, args.cmap)
+except AttributeError:
+    print('matplotlib colormap \'{}\' not found'.format(args.cmap))
+    sys.exit(1)
 
-for n in np.linspace(0.0, 1.0, 256):
-    print('{} {} {}'.format(*map(int, np.array(cmap(n)[:3])*255)))
+nbands = 10
+bandwidth = 5 # odd
+
+xs = np.linspace(0, 1, 256)
+if args.bands:
+    bandi = np.round(np.linspace(0, 255, nbands+2))[1:-1].astype(np.int)
+    offsets = list(range(-(bandwidth//2),(bandwidth//2)+1))
+    halfband = np.linspace(.10, .30, bandwidth//2)
+    mixing = np.r_[halfband, .60, halfband[::-1]]
+    whites = np.zeros_like(xs)
+    blacks = np.zeros_like(xs)
+    for off, mix in zip(offsets, mixing):
+        whites[bandi[::2] + off] = mix
+        blacks[bandi[1::2] + off] = mix
+
+for i, n in enumerate(xs):
+    rgb = np.array(cmap(n)[:3])
+    if args.bands:
+        hls = colorsys.rgb_to_hls(*rgb)
+        rgb = np.array(colorsys.hls_to_rgb(
+            hls[0],
+            (hls[1] + 1*whites[i] + 0*blacks[i])/(1 + whites[i] + blacks[i]),
+            hls[2]))
+    print('{} {} {}'.format(*map(int, rgb*255)))
+
+sys.exit(0)
